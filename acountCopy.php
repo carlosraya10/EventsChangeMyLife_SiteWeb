2624786c0b1e2678634f19475d0812707dcfcafd
ioanaUngureanu <ioanazib@yahoo.com>
2015-02-05 15:11:02 +0100

    modif header + menu
    Carlos, pour mettre le header a part et le menu a part il faut faire des
    pages .php qu'on va include dans notre page qui elle aussi doit etre
    .php et non pas .html. du coup j'ai fait acountCopy.php headerCopy.php
    et menuCopy.php pour que tu voies comment faire et tu peux tester avec
    wamp. voila ! ensuite on se mettra d'accord qui modifie quoi parce qu'il
    a y a eu un </div> manquant de la partie manu a toi qui m'a pris
    longtemps pour trouver cette erreur. hihi...

---
+++ acountCopy.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>BOX SHOP an E-Commerce online Shopping Category Flat Bootstarp responsive Website Template| Account :: w3layouts</title>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="BOX SHOP Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--webfont-->
+<link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+<!-- start menu -->
+<link href="css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/megamenu.js"></script>
+<script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
+<!-- start slider -->
+<script src="js/responsiveslides.min.js"></script>
+ <script>
+    $(function () {
+      $("#slider").responsiveSlides({
+      	auto: true,
+      	speed: 500,
+        namespace: "callbacks",
+        pager: true,
+      });
+    });
+  </script>
+<!--end slider -->
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+				<script type="text/javascript">
+			$(window).load(function() {
+				$("#flexiselDemo").flexisel({
+					visibleItems: 5,
+					animationSpeed: 1000,
+					autoPlay: true,
+					autoPlaySpeed: 3000,    		
+					pauseOnHover: true,
+					enableResponsiveBreakpoints: true,
+			    	responsiveBreakpoints: { 
+			    		portrait: { 
+			    			changePoint:480,
+			    			visibleItems: 1
+			    		}, 
+			    		landscape: { 
+			    			changePoint:640,
+			    			visibleItems: 2
+			    		},
+			    		tablet: { 
+			    			changePoint:768,
+			    			visibleItems: 3
+			    		}
+			    	}
+			    });
+			    
+			});
+		</script>
+		<script type="text/javascript" src="js/jquery.flexisel.js"></script>
+</head>
+<body>
+	<?php include("headerCopy.php"); ?>
+	
+	
+	<div class="wrap">
+		<!-- start header menu -->
+		<?php include("menuCopy.php"); ?>
+
+		<!-- start account -->
+		<div class="login-page">
+			    <div class="dreamcrub">
+			   <div class="account_grid">
+			   <div class="col-md-6 login-left">
+			  	 <h3>NEW CUSTOMERS</h3>
+				 <p>By creating an account with our store, you will be able to move through the checkout process faster, store multiple shipping addresses, view and track your orders in your account and more.</p>
+				 <a class="acount-btn" href="register.html">Create an Account</a>
+			   </div>
+			   <div class="col-md-6 login-right">
+			  	<h3>REGISTERED CUSTOMERS</h3>
+				<p>If you have an account with us, please log in.</p>
+				<form>
+				  <div>
+					<span>Email Address*</span>
+					<input type="text"> 
+				  </div>
+				  <div>
+					<span>Password*</span>
+					<input type="text"> 
+				  </div>
+				  <p class="required">* Required Fields</p>
+				  <a class="forgot" href="#">Forgot Your Password?</a>
+				  <input type="submit" value="Login">
+			    </form>
+			   </div>	
+			   <div class="clearfix"> </div>
+			 </div>
+		   </div>
+</div>
+	</div>
+	 <div class="footer">
+		<div class="wrap">
+			<div class="contact-section">
+				<div class="col-md-4 follow text-left">
+					<h3>Follow Us</h3>
+					<p>Lorem ipsum dolor sit amet</p>
+					<div class="social-icons">
+						<i class="twitter"></i>
+						<i class="facebook"></i>
+						<i class="googlepluse"></i>
+						<i class="pinterest"></i>
+						<i class="linkedin"></i>
+					</div>
+				</div>
+				<div class="col-md-4 subscribe text-left">
+					<h3>Subscribe Us</h3>
+					<p>Get the latest updates & Offers right in your inbox.</p>
+					<input type="text" class="text" value="" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = '';}">
+					<input type="submit" value="Subscribe">
+				</div>
+				<div class="col-md-4 help text-right">
+					<h3>Need Help?</h3>
+					<p>Lorem ipsum dolor sit amet</p>
+					<a href="contact.html">Contact us</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="footer-middle">
+				<div class="col-md-6 different-products">
+					<ul>
+						<li class="first"> Shop </li> -
+						<li><a href=""> Mobiles </a></li> |
+						<li><a href=""> Laptops </a></li> |
+						<li><a href=""> Cameras </a></li> |
+						<li><a href=""> Clothing </a></li> |
+						<li><a href=""> Footwear </a></li> |
+						<li><a href=""> Jewellery </a></li> 
+					</ul>
+					<ul>
+						<li class="first"> Help </li> -
+						<li><a href=""> Faqs </a></li> |
+						<li><a href=""> shipping </a></li> |
+						<li><a href=""> payments </a></li> |
+						<li><a href=""> cancellation&returns </a></li> 
+					</ul>
+					<ul>
+						<li class="first"> account <li> -
+						<li><a href=""> log in </a></li> |
+						<li><a href=""> sign up </a></li> |
+						<li><a href=""> My WhishList </a></li> |
+						<li><a href=""> My cart </a></li> 
+					</ul>
+					<ul>
+						<li class="first"> boxshop </li> -
+						<li><a href=""> contact us </a></li> |
+						<li><a href=""> about us </a></li> |
+						<li><a href=""> careers </a></li> |
+						<li><a href=""> blog </a></li> |
+						<li><a href=""> press </a></li>
+					</ul>
+					<ul>
+						<li class="first"> policies</li> -
+						<li><a href=""> terms of use </a></li> |
+						<li><a href=""> security </a></li> |
+						<li><a href=""> privacy policy</a></li>
+					</ul>
+				</div>
+				<div class="col-md-6 about-text text-right">
+					<h4>About BoxShop</h4>
+					<p>The fashion never alters, and as it is neither disagreeable nor uneasy, so it is suited to the climate, and calculated both for their summers and winters. Every family makes their own clothes; but all among them, women as well as men, learn one or other of the trades formerly mentioned.</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="cards text-center">
+				<img src="images/cards.jpg" alt="" />
+			</div>
+			<div class="copyright text-center">
+				<p>Copyright &copy; 2015 All rights reserved | Template by  <a href="http://w3layouts.com">  W3layouts</a></p>
+			</div>
+
+		</div>
+	 </div>
+</body>
+</html>
---
+++ menuCopy.php
@@ -0,0 +1,140 @@
+		<div class="navigation-strip">
+			<h4>Catégories<i class="arrow"></i></h4>
+			<div class="top-menu">
+
+<!-- start header menu -->
+		<ul class="megamenu skyblue">
+			<li><a class="color1" href="#">animation</a>
+				<div class="megapanel">
+					<div class="row">
+						<div class="col1">
+							<div class="h_nav">
+								<h4>animateur</h4>
+								<ul>
+									<li><a href="products.html"> enfants</a></li>
+									<li><a href="products.html"> micro</a></li>
+									<li><a href="products.html"> polyvalent</a></li>
+									<li><a href="products.html"> sportif</a></li>
+								</ul>	
+							</div>							
+						</div>
+					</div>
+    			</div>
+			</li>
+			<li class="grid"><a class="color2" href="#">acteurs</a>
+				<div class="megapanel">
+					<div class="row">
+						<div class="col1">
+							<div class="h_nav">
+								<h4>----------</h4>
+								<ul>
+									<li><a href="products.html">comédiens</a></li>
+									<li><a href="products.html">théâtre</a></li>
+								</ul>	
+							</div>							
+						</div>
+					</div>
+    			</div>
+			</li>
+			<li class="grid"><a class="color3" href="#">danse</a>
+				<div class="megapanel">
+					<div class="row">
+						<div class="col1">
+							<div class="h_nav">
+								<h4> danseur </h4>
+								<ul>
+									<li><a href="products.html"> africain</a></li>
+									<li><a href="products.html"> chorégraphe</a></li>
+									<li><a href="products.html"> country music</a></li>
+									<li><a href="products.html"> hip-hop</a></li>
+									<li><a href="products.html"> indou</a></li>
+									<li><a href="products.html"> jazz-dance</a></li>
+									<li><a href="products.html"> oriental</a></li>
+									<li><a href="products.html">salsa</a></li>
+								</ul>	
+							</div>							
+						</div>
+					</div>
+    			</div>
+			</li>
+			<li class="grid"><a class="color4" href="#">music</a>
+				<div class="megapanel">
+					<div class="row">
+						<div class="col1">
+							<div class="h_nav">
+								<h4> ---------- </h4>
+								<ul>
+									<li><a href="products.html"> chanteur</a></li>
+									<li><a href="products.html"> DJ</a></li>
+									<li><a href="products.html"> fanfare</a></li>
+									<li><a href="products.html"> groupes</a></li>
+									<li><a href="products.html"> orchestres</a></li>
+								</ul>
+							</div>							
+						</div>
+					</div>
+    			</div>
+			</li>
+			<li class="grid"><a class="color5" href="#">spectacle de rue</a>
+				<div class="megapanel">
+					<div class="row">
+						<div class="col1">
+							<div class="h_nav">
+								<h4> ---------- </h4>
+								<ul>
+									<li><a href="products.html"> acrobate</a></li>
+									<li><a href="products.html"> clown</a></li>
+									<li><a href="products.html"> contorsionniste</a></li>
+									<li><a href="products.html"> cracheurs de feu</a></li>
+									<li><a href="products.html"> échassiers</a></li>
+									<li><a href="products.html"> illusionniste</a></li>
+									<li><a href="products.html"> jongleur</a></li>
+									<li><a href="products.html"> mime</a></li>
+									<li><a href="products.html"> sculpture de ballons</a></li>
+								</ul>
+							</div>							
+						</div>
+					</div>
+    			</div>
+			</li>
+			<li class="grid"><a class="color6" href="#">maquillage</a>
+				<div class="megapanel">
+					<div class="row">
+						<div class="col1">
+							<div class="h_nav">
+								<h4> maquilleuse </h4>
+								<ul>
+									<li><a href="products.html"> adultes</a></li>
+									<li><a href="products.html"> enfants</a></li>
+								</ul>
+							</div>							
+						</div>
+					</div>
+    			</div>
+			</li>
+			<li class="grid"><a class="color7" href="#">autres</a>
+				<div class="megapanel">
+					<div class="row">
+						<div class="col1">
+							<div class="h_nav">
+								<h4> ---------- </h4>
+								<ul>
+									<li><a href="products.html">  traiteur</a></li>
+									<li><a href="products.html"> décoration</a></li>
+									<li><a href="products.html"> fleuriste</a></li>
+									<li><a href="products.html"> imprimerie</a></li>
+									<li><a href="products.html"> location salle</a></li>
+									<li><a href="products.html"> pâtissier</a></li>
+									<li><a href="products.html"> photographe</a></li>
+									<li><a href="products.html"> sécurité</a></li>
+									<li><a href="products.html"> son & lumière</a></li>
+									<li><a href="products.html"> Traiteur</a></li>
+								</ul>
+							</div>							
+						</div>
+					</div>
+    			</div>
+			</li>
+			</ul>
+			</div>
+		<div class="clearfix"></div>
