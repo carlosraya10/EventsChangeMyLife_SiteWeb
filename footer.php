16299ce926d261b34a00db5fde0de0c90121633a
carlosraya10 <carlosraya20@hotmail.com>
2015-02-13 17:35:54 +0100

    PAGE D'ACCUEIL
    Ioana je iens de mettre les fichier php pour pouvoir gérer plus
    facilement les paties (menu, header, footer et "contenu") Notre index
    c'est maintnant le index.php et l'index anterieur c'est
    "index_ioana.html" je l'ai pas supprimé si toujours tu veux tout le code
    dans un seul fichier, mais je l'ai déjà mis sur home.php, donc je pense
    qu'on n'en a plus besoin.

---
+++ footer.php
@@ -0,0 +1,82 @@
+<div class="footer">
+		<div class="wrap">
+			<div class="contact-section">
+				<div class="col-md-4 follow text-left">
+					<h3>Suivez-nous</h3>
+					<p>Lorem ipsum dolor sit amet</p>
+					<div class="social-icons">
+						<i class="twitter"></i>
+						<i class="facebook"></i>
+						<i class="googlepluse"></i>
+						<i class="pinterest"></i>
+						<i class="linkedin"></i>
+					</div>
+				</div>
+				<div class="col-md-4 subscribe text-left">
+					<h3>Abonnez-vous</h3>
+					<p>Recevez les news des toutes les fêtes ...</p>
+					<input type="text" class="text" value="" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = '';}">
+					<input type="submit" value="Subscribe">
+				</div>
+				<div class="col-md-4 help text-right">
+					<h3>Besoin d'aide ?</h3>
+					<p>Lorem ipsum dolor sit amet</p>
+					<a href="contact.html">Contactez-nous</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="footer-middle">
+				<div class="col-md-6 different-products">
+					<ul>
+						<li class="first"> Shop </li> -
+						<li><a href=""> Mobiles </a></li> |
+						<li><a href=""> Laptops </a></li> |
+						<li><a href=""> Cameras </a></li> |
+						<li><a href=""> Clothing </a></li> |
+						<li><a href=""> Footwear </a></li> |
+						<li><a href=""> Jewellery </a></li> 
+					</ul>
+					<ul>
+						<li class="first"> Help </li> -
+						<li><a href=""> Faqs </a></li> |
+						<li><a href=""> shipping </a></li> |
+						<li><a href=""> payments </a></li> |
+						<li><a href=""> cancellation&returns </a></li> 
+					</ul>
+					<ul>
+						<li class="first"> account <li> -
+						<li><a href=""> log in </a></li> |
+						<li><a href=""> sign up </a></li> |
+						<li><a href=""> My WhishList </a></li> |
+						<li><a href=""> My cart </a></li> 
+					</ul>
+					<ul>
+						<li class="first"> boxshop </li> -
+						<li><a href="contact.html"> contact us </a></li> |
+						<li><a href=""> about us </a></li> |
+						<li><a href=""> careers </a></li> |
+						<li><a href=""> blog </a></li> |
+						<li><a href=""> press </a></li>
+					</ul>
+					<ul>
+						<li class="first"> policies</li> -
+						<li><a href=""> terms of use </a></li> |
+						<li><a href=""> security </a></li> |
+						<li><a href=""> privacy policy</a></li>
+					</ul>
+				</div>
+				<div class="col-md-6 about-text text-right">
+					<h4>Events Change Life</h4>
+					<p> Une plateforme centrale de réservation destinée à l'événementiel pour permettre aussi aux professionnels qu’aux amateurs de  mettre en avant leurs compétences ou services afin de se faire valoir. </p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="cards text-center">
+				<img src="images/cards.jpg" alt="" />
+			</div>
+			<div class="copyright text-center">
+				<p>Copyright &copy; 2015 All rights reserved | ECL </p>
+			</div>
+
+		</div>
+	 </div>
---
+++ header.php
@@ -0,0 +1,52 @@
+<!-- header-section-starts -->
+	<div class="header">
+		<div class="top-header">
+			<div class="wrap">
+				<div class="header-left">
+					<ul>
+						<li><a href="#">24x7 Service Client </a></li> |
+						<li><a href="order.html"> Suivi commande </a></li>
+					</ul>
+				</div>
+				<div class="header-right">
+					<ul>
+						<li>
+							<i class="user"></i>
+							<a href="account.html">Mon compte</a>
+						</li>
+						<li class="login">
+							<i class="lock"></i>
+							<a href="login.html">Se connecter</a>|
+							<a href="login.html">S'identifier</a>			
+						</li>
+						<li>
+							<i class="cart"></i>
+							<a href="#">Mon Panier</a>
+						</li>
+						<li class="last">5</li>
+					</ul>					
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+			
+		<div class="wrap">
+			<div class="header-bottom">
+				<div class="logo">
+					<a href="index.html"><img src="images/logo.png" class="img-responsive" alt="" /></a>
+				</div>
+				<div class="search">
+					<div class="search2">
+					  <form>
+						<input type="submit" value="">
+						 <input type="text" value="Rechercher un prestataire" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Search for a product, category or brand';}"/>
+					  </form>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+	
+	
+	<!-- header-section-ends -->
