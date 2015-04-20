c8c047e3bd1fbea0c3599ae753b4bfde79cd8878
carlosraya10 <carlosraya10@gmail.com>
2015-04-20 17:02:48 +0200

    headermenu

---
+++ php/headerMenu.php
@@ -0,0 +1,214 @@
+
+<div class="wrap">
+		<div class="navigation-strip">
+			<h4>Catégories<i class="arrow"></i></h4>
+			<div class="top-menu">
+			
+<ul class="megamenu skyblue">
+			<li><a class="color1" href="#">animation</a>
+				<div class="megapanel">
+					<div class="row">
+						<div class="col1">
+							<div class="h_nav">
+								<h4>animateur</h4>
+								<ul>
+									<li><a href="produitsAnimation.php"> commercial</a></li>
+									<li><a href="produitsAnimation.php"> de vente</a></li>
+									<li><a href="produitsAnimation.php"> enfants</a></li>
+									<li><a href="produitsAnimation.php"> micro divertissment</a></li>
+									<li><a href="produitsAnimation.php"> polyvalent</a></li>
+									<li><a href="produitsAnimation.php"> socio-culturel</a></li>
+									<li><a href="produitsAnimation.php"> sportif</a></li>
+								</ul>	
+							</div>							
+						</div>
+						<div class="col1">
+							<div class="h_nav">
+								<h4>Animation fixe</h4>
+								<ul>
+									<li><a href="produitsAnimation.php"> caricaturiste</a></li>
+									<li><a href="produitsAnimation.php"> char</a></li>
+									<li><a href="produitsAnimation.php"> mascottes</a></li>
+									<li><a href="produitsAnimation.php"> maquillage enfant</a></li>
+									<li><a href="produitsAnimation.php"> princesses</a></li>
+									<li><a href="produitsAnimation.php"> sportif</a></li>
+									<li><a href="produitsAnimation.php"> structures gonflables</a></li>
+								</ul>	
+							</div>							
+						</div>
+					</div>
+    			</div>
+			</li>
+			<li class="grid"><a class="color2" href="#">Spectacle</a>
+				<div class="megapanel">
+					<div class="row">
+						<div class="col1">
+							<div class="h_nav">
+								<h4>Acteurs</h4>
+								<ul>
+									<li><a href="products.html">comédiens</a></li>
+									<li><a href="products.html">figurants</a></li>
+									<li><a href="products.html">thèâtre</a></li>
+								</ul>	
+							</div>							
+						</div>
+						<div class="col1">
+							<div class="h_nav">
+								<h4>Danseur</h4>
+								<ul>
+									<li><a href="products.html"> africain</a></li>
+									<li><a href="products.html"> chorégraphe</a></li>
+									<li><a href="products.html"> country music</a></li>
+									<li><a href="products.html"> hip-hop</a></li>
+									<li><a href="products.html"> indou</a></li>
+									<li><a href="products.html"> jazz-dance</a></li>
+									<li><a href="products.html"> oriental</a></li>
+									<li><a href="products.html"> salsa</a></li>
+								</ul>	
+							</div>							
+						</div>
+						<div class="col1">
+							<div class="h_nav">
+								<h4>Spectacle de Rue</h4>
+								<ul>
+									<li><a href="products.html"> acrobate</a></li>
+									<li><a href="products.html"> clown</a></li>
+									<li><a href="products.html"> contorsionniste</a></li>
+									<li><a href="products.html"> cracheurs de feu</a></li>
+									<li><a href="products.html"> échassiers</a></li>
+									<li><a href="products.html"> illusionniste</a></li>
+									<li><a href="products.html"> jongleur</a></li>
+									<li><a href="products.html"> mime</a></li>
+									<li><a href="products.html"> sculpture de ballons</a></li>
+								
+								</ul>	
+							</div>							
+						</div>
+						
+						<div class="col1">
+							<div class="h_nav">
+								<h4>Music</h4>
+								<ul>
+									<li><a href="products.html"> chanteur</a></li>
+									<li><a href="products.html"> DJ</a></li>
+									<li><a href="products.html"> fanfare</a></li>
+									<li><a href="products.html"> groupes</a></li>
+									<li><a href="products.html"> orchestres</a></li>
+								
+								</ul>	
+							</div>							
+						</div>
+					</div>
+    			</div>
+			</li>
+			<li class="grid"><a class="color3" href="#">Autres besoins</a>
+				<div class="megapanel">
+					<div class="row">
+						<div class="col1">
+							<div class="h_nav">
+								<h4> Restauration </h4>
+								<ul>
+									<li><a href="products.html"> boulanger</a></li>
+									<li><a href="products.html"> patissier</a></li>
+									<li><a href="products.html"> serveur</a></li>
+									<li><a href="products.html"> traiteur</a></li>
+								</ul>	
+							</div>							
+						</div>
+						
+						<div class="col1">
+							<div class="h_nav">
+								<h4> Decoration </h4>
+								<ul>
+									<li><a href="products.html"> decorateur</a></li>
+									<li><a href="products.html"> fleuriste</a></li>
+									
+								</ul>	
+							</div>							
+						</div>
+						
+						<div class="col1">
+							<div class="h_nav">
+								<h4> Esthètique </h4>
+								<ul>
+									<li><a href="products.html"> coiffure</a></li>
+									<li><a href="products.html"> maquillage adultes</a></li>
+									
+								</ul>	
+							</div>							
+						</div>
+						
+						<div class="col1">
+							<div class="h_nav">
+								<h4> Divers </h4>
+								<ul>
+									<li><a href="products.html"> feu d'artifice</a></li>									
+									<li><a href="products.html"> imprimerie</a></li>
+																		
+									<li><a href="products.html"> photographe</a></li>
+									<li><a href="products.html"> sécurité</a></li>
+									<li><a href="products.html"> son & lumiére</a></li>
+															
+								</ul>	
+							</div>							
+						</div>
+					</div>
+    			</div>
+			</li>		
+			
+			
+			<li class="grid"><a class="color7" href="#">Locations</a>
+				<div class="megapanel">
+					<div class="row">
+						<div class="col1">
+							<div class="h_nav">
+								<h4>Locations</h4>
+								<ul>
+									<li><a href="products.html"> Location salle</a></li>
+									<li><a href="products.html"> Location de chapiteau</a></li>
+									<li><a href="products.html"> Location de mobilier</a></li>
+									<li><a href="products.html"> Location de sono</a></li>
+									<li><a href="products.html"> Location de vehicules</a></li>	
+								</ul>
+							</div>							
+						</div>
+					</div>
+    			</div>
+			</li>
+			
+			<li class="grid"><a class="color7" href="#">unknown</a>
+				<div class="megapanel">
+					<div class="row">
+						<div class="col1">
+							<div class="h_nav">
+								<h4> ---------- </h4>
+								<ul>
+									
+									</ul>
+							</div>							
+						</div>
+					</div>
+    			</div>
+			</li>
+			
+			<li class="grid"><a class="color7" href="#">unknown</a>
+				<div class="megapanel">
+					<div class="row">
+						<div class="col1">
+							<div class="h_nav">
+								<h4> ---------- </h4>
+								<ul>
+									
+									</ul>
+							</div>							
+						</div>
+					</div>
+    			</div>
+			</li>
+			
+			</ul>
+			</div>
+		<div class="clearfix"></div>
+</div>
+
+	
